4887f08df5b785ef1a89fadba64c8301451ffa33
Konstantin Duzenko <konstantin.duzenko@rif-m.ru>
2020-12-26 21:26:12 +0300

    first commit

---
+++ ReadRDS.py
@@ -0,0 +1,59 @@
+
+
+# import struct
+
+# fmt="<HI1s1sB" #два
+# # байтовых символов 
+# BLOCK_SIZE=struct.calcsize(fmt)
+# #assert BLOCK_SIZE==8
+
+# with open("C:/KOS/temp_log.rdf","rb") as f:
+# 	tmpbuf = f.read()
+    
+# 	counter, time, text1, text2, state = struct.unpack_from(fmt, tmpbuf,9)
+    
+# 	#print(f"{counter}\t{time}\t{text1.decode('utf-8')}")
+import time
+
+# import numpy as np
+# bufType=np.dtype(
+# 	[
+# 		('counter', 'i2'),
+# 		('time', 'i4'),
+# 		('text_1', 'S1'),
+#         ('text_2', 'S1'),
+#         ('state', 'i1')
+# 	]
+# )
+
+# data = np.fromfile('C:/KOS/PyOsc/1.rdf', dtype=bufType,offset=30486)
+
+# print(data)
+# print(time.localtime(data[1]['time']))
+
+import numpy as nposc
+bufType=nposc.dtype(
+	[
+		('Ia1', 'i2'),
+		('Ic1', 'i2'),
+		('Ia2', 'i2'),
+        ('Ic2', 'i2'),
+        ('Iakt', 'i2'),
+        ('Ickt', 'i2'),
+		('Uab1', 'i2'),
+		('Uab2', '<i2'),
+      
+        ('osc29', 'u1'),
+        ('osc30', 'u1'),
+        ('osc31', 'u1'),
+        ('osc32', 'u1')
+        ]
+)
+
+osc = np.fromfile('C:/KOS/PyOsc/1.rdf', dtype=bufType,count=1000,offset=34)
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(osc['osc32'])
+plt.show()
---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+import os
+import openpyxl
+import sys
+
+root_path = "C:\Users\Владелец\Desktop\q"
+param_1 = "kt_i_max_down"
+param_2 = "kt-imax_down"
+
+def GetFiles(path):
+    files_ = list()
+    for root, dirs, files in os.walk(path):
+       for name in files:
+          file = os.path.join(root, name)
+          files_.append(file)
+    return files_
+
+if __name__ == '__main__':
+        if True:
+            files = GetFiles(root_path)        
+            for file in files:
+                if file.split(".")[1] == "csv":
+                    file_1 = open(file,"r")
+                    buffer = file_1.read()
+                    new_buffer = buffer.replace(param_1,param_2)
+                    file_1.close()
+                    file_2 = open(file, 'w')
+                    file_2.write(new_buffer)
+                    file_2.close()
+                if file.split(".")[1] == "xlsm":
+                    wb = openpyxl.load_workbook(file,keep_vba=True)
+                    for sheets in wb:
+                        for sheet in sheets:
+                            for end in sheet:
+                                if end.value == param_1:
+                                    end.value = param_2
+                                    print(".")
+                    wb.save(file)
+        print("Ok")   
