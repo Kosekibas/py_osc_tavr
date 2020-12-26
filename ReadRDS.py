8bd06ec235bc5f3480753a8060146f4c60b7aef0
Kosekibas <Kosekibas@gmail.com>
2020-12-27 01:24:44 +0300

    Delet Old comment

--- ReadRDS.py
+++ ReadRDS.py
@@ -1,35 +1,7 @@
 
 
-# import struct
-
-# fmt="<HI1s1sB" #два
-# # байтовых символов 
-# BLOCK_SIZE=struct.calcsize(fmt)
-# #assert BLOCK_SIZE==8
-
-# with open("C:/KOS/temp_log.rdf","rb") as f:
-# 	tmpbuf = f.read()
-    
-# 	counter, time, text1, text2, state = struct.unpack_from(fmt, tmpbuf,9)
-    
-# 	#print(f"{counter}\t{time}\t{text1.decode('utf-8')}")
 import time
 
-# import numpy as np
-# bufType=np.dtype(
-# 	[
-# 		('counter', 'i2'),
-# 		('time', 'i4'),
-# 		('text_1', 'S1'),
-#         ('text_2', 'S1'),
-#         ('state', 'i1')
-# 	]
-# )
-
-# data = np.fromfile('C:/KOS/PyOsc/1.rdf', dtype=bufType,offset=30486)
-
-# print(data)
-# print(time.localtime(data[1]['time']))
 
 import numpy as nposc
 bufType=nposc.dtype(
@@ -42,7 +14,7 @@ bufType=nposc.dtype(
         ('Ickt', 'i2'),
 		('Uab1', 'i2'),
 		('Uab2', '<i2'),
-              ('Dio', 'u4')
+        ('Dio', 'u4')
         ]
 )
 
@@ -59,25 +31,14 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(nrows=32, ncols=1)
 
 n = 1
-for ax in axes.flat:
-    
+for ax in axes.flat:  
     ax.set(xticks=[], yticks=[])
     ax.set_ylim(ymin=-0.1,ymax=1.1)
     ax.set_xlim(xmin=0,xmax=1500)
     ax.text(-100, 0.5, 'DIO_' + str(n), color='red')
     ax.stem ( ~(osc['Dio'] >> n-1) & 1 , linefmt='C0-', markerfmt='C0-', basefmt='C0-') 
-    
     n += 1
-    
 plt.show()
 
 #save('pic_7_2_1', fmt='png')
 #save('pic_7_2_1', fmt='pdf')
-
-
-#!!
-
-#plt.plot(osc['Dio'])
-#print (dio)
-#plt.plot(dio)
-#plt.show()
