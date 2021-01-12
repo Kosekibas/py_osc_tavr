f38de1f38d85b2e0438b9d8145b4fa7b80fbc203
Kosekibas <Kosekibas@gmail.com>
2021-01-12 20:08:35 +0300

    первая версия подбора номера тавр к осцилограмме

--- import csv.py
+++ import csv.py
@@ -1,37 +1,49 @@
 import pandas as pd
 import os
-import openpyxl
+#import openpyxl
 #import sys
 
 
-root_path = "C:\\Users\\Владелец\\Desktop\\Examples\\python\\Osc\\"
+#root_path = "C:\\Users\\Владелец\\Desktop\\Examples\\python\\Osc\\"
+root_path = "E:\\KosPy\\Samples\\py_osc_tavr\\Osc\\"
 
-def GetFiles(path):
-    files_ = pd.DataFrame( columns=['root', 'name'])
-    for root, dirs, files in os.walk(path):
-        for name in files:
+def Getfiles_osc(path):
+    files_osc_ = pd.DataFrame( columns=['root', 'name','number'])
+    for root, dirs, files_osc in os.walk(path):
+        for name in files_osc:
             if name.split(".")[1] == "rdf":
                 #file = os.path.join(root, name)
-                # files_.append(file)
-                files_.loc[name]=root[root,name]
+                # files_osc_.append(file)
+                files_osc_.loc[files_osc_.shape[0]]=[root,name,0]
                 
-    return files_
+    return files_osc_
 
 
 # получение номеров тавров
 # ! Завернуть в функцию
-data=pd.read_csv(
+all_tavr=pd.read_csv(
     "SN_TAVR.csv",
     delimiter=';',
     names=['serial_number','station'] )
-print(data)
+print(all_tavr)
+
+
+     
+
+files_osc = Getfiles_osc(root_path)   
+print(files_osc)
 
 
 # цикл сравнения
-#if __name__ == '__main__':
-#    for idx  in data.itertuples():
-#        if idx.station=='Песь':
-#            print(idx.serial_number)
-#      
-
-files = GetFiles(root_path)   
+if __name__ == '__main__':
+    for tavr  in all_tavr.itertuples(): # перебор станций по массиву номеров тавр
+         for osc  in files_osc.itertuples(): # перебор путей найденных файлов осцилограм
+            folder_name=str.split(osc.root, sep='\\') # разбивка пути файла на названия папок
+            for name in folder_name: #перебор названий каждой папки
+                if tavr.station==name: # если совпало название папки и название станции
+                    print('YES!')
+                    print(tavr.serial_number)
+                    files_osc.number[osc.Index]=tavr.serial_number #!! записываем в массив номер тавра
+                    print(files_osc)
+    print('End')
+ #todo экспорт в csv файл
