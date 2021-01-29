68a9a76d72b21902a04cddb91cee2b57912f9c86
Kosekibas <kosekibas@gmail.com>
2021-01-29 14:37:08 +0300

    засунул под функцию парсинг в словарь питона

--- parser_byte_to_byte.py
+++ parser_byte_to_byte.py
@@ -2,8 +2,9 @@
 # todo побайтно проверять данные и закидывать в словарь
 
 
-import pandas as pd
+
 import os
+import pandas as pd
 import numpy as np
 import  time # для конвертирования времени
 
@@ -23,20 +24,15 @@ dict_vers = {
     }
 
 dict_graph = {} #пустой словарь настроек отображения графиков
-style = { #словарь свойств линий графиков
-    'pbChannels':{},
-    'pcrColor':{},
-    'piLineWidth':{},
-    'piLineStyle':{}
-}
-dict_msg = {} #пустой словарь настроек отображения графиков
-msg = { #словарь свойств линий графиков
-    'num':0,
-    'time':0,
-    'msg':0,
-    'style':0,
-    'code':0
-}
+# style = { #словарь свойств линий графиков
+#     'pbChannels':{},
+#     'pcrColor':{},
+#     'piLineWidth':{},
+#     'piLineStyle':{}
+# }
+
+#пустой словарь настроек отображения графиков
+
 
 dict_code={
     0:'СЕКЦИOHHЫЙ BKЛЮЧЕH',
@@ -90,84 +86,139 @@ dict_code={
 }
 dict_osc={} #словарь всей осицлограммы
 
-def Getfiles_osc(path): # Получение списка осциллограмм
-    files_osc_ = pd.DataFrame( columns=['root', 'name'])
-    for root,  files_osc in os.walk(path):
-        for name in files_osc:
-            name_list=name.split(".")
-            if name_list[len(name_list)-1] == "rdf": #сравнение именно последнего расширения после точки, отсеивается rdf.bak и прочее
-                # size=os.path.getsize(root+'\\'+name) # получить информацию и размере файла
-                files_osc_.loc[files_osc_.shape[0]]=[root,name]
-    return files_osc_
+def Getfiles(path,extension): # Получение списка файлов в папке
+    files_ = pd.DataFrame( columns=['root', 'name'])
+    for root, dirnames , filenames in os.walk(path):
+        for name_full in filenames:
+            name=name_full.split(".")
+            if name[len(name)-1] == extension: #сравнение именно последнего расширения после точки, отсеивается rdf.bak и прочее
+                files_.loc[files_.shape[0]]=[root,name_full]
+    return files_
 
-# files_osc = Getfiles_osc(root_path)   #составления списка осцилограмм
 
-def Parsing_osc ()
-files_osc = Getfiles_osc(root_path)   #составления списка осцилограмм
-for osc  in files_osc.itertuples(): # перебор путей найденных файлов осцилограм
-    with open (osc.root+osc.name, 'rb') as f_osc:
-        #! засунуть проверку на вылет, когда не может декодировать rdf
-        dict_vers['extension']=f_osc.read (4).decode('ANSI')
-        # print("формат=",dict_vers['extension'])
-        if dict_vers['extension']=="RDF":
-            dict_vers['v1']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['v2']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['v3']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['v4']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['v5']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['v6']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            dict_vers['time']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-            # print(dict_vers)
-            log=f_osc.read (30050)
-            # сообщение в заголовке
-            title_log=int.from_bytes(f_osc.read (1),byteorder='little',signed=0) #длинна байт
-            title_msg=f_osc.read (title_log).decode(encoding='ANSI') # само сообщение
-            # отображение графиков
-            osc_graph_format_string_count=int.from_bytes(f_osc.read (4),byteorder='little',signed=0) #читаем количество отоброжаемых окон графиков
-            for n in range(osc_graph_format_string_count): 
-                osc_graph_format_line_count = int.from_bytes(f_osc.read (4),byteorder='little',signed=0) #количество графиков в окне
-                for m in range(osc_graph_format_line_count):
-                    dict_graph[n]=style
-                    dict_graph[n]['pbChannels'][m]=int.from_bytes(f_osc.read (1),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].pbChannels[j]; 1b
-                    dict_graph[n]['pcrColor'][m]=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)     # ar>>pView->pCustomPD[i].pcrColor[j];    4b
-                    dict_graph[n]['piLineWidth'][m]=int.from_bytes(f_osc.read (4),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].piLineWidth[j]; 4b
-                    dict_graph[n]['piLineStyle'][m]=int.from_bytes(f_osc.read (4),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].piLineStyle[j]; 4b
-                    # print(  'nn=',n,
-                    #         'mm=',m, 
-                    #         'pbChannels',(dict_graph[n]['pbChannels'][m]),
-                    #         'pcrColor', dict_graph[n]['pcrColor'][m],
-                    #         'piLineWidth', dict_graph[n]['piLineWidth'][m],
-                    #         'piLineStyle', dict_graph[n]['piLineStyle'][m] 
-                    #         )
-                fDigOnly=f_osc.read (4)  # ar>>pView->pCustomPD[i].fDigOnly;   4b
-                rect=f_osc.read (16)       # ar>>pView->pCustomPD[i].rect;       4*4b
-            m_fCaptionEnable=f_osc.read (4) # ar>>pView->m_fCaptionEnable;
-            m_fLegendEnable=f_osc.read (4)  # ar>>pView->m_fLegendEnable;
-            m_iTotalKadr=f_osc.read (4)    # ar>>m_iTotalKadr;
-            iAmountKadr=f_osc.read (4)  # ar>>ViewData.iAmountKadr;
-            iFromKadr= f_osc.read (4) # ar>>ViewData.iFromKadr;
-            m_iMessageCount=int.from_bytes(f_osc.read (4),byteorder='little',signed=0) #количество сообщений
-            print('количество сообщений= ',m_iMessageCount) 
-            for m in range(m_iMessageCount):
-                dict_msg[m]=msg
-                dict_msg[m]['num']=int.from_bytes(f_osc.read (2),byteorder='little',signed=0)
-                dict_msg[m]['time']=int.from_bytes(f_osc.read (4),byteorder='little',signed=0)
-                dict_msg[m]['msg']=f_osc.read (1).decode('ANSI')
-                dict_msg[m]['style']=f_osc.read (1).decode('ANSI')
-                dict_msg[m]['code']=dict_code[int.from_bytes(f_osc.read (1),byteorder='little',signed=0 )]
-                if dict_msg[m]['time'] == dict_vers['time']:
-                    event_counter=1
-                    event= dict_msg[m]['code']
-                elif dict_msg[m]['time']+1 == dict_vers['time']:
-                    event_counter=2
-                    event= dict_msg[m]['code']
-                elif dict_msg[m]['time']+2 == dict_vers['time']:
-                    event_counter=3
-                    event= dict_msg[m]['code']
-                else:
-                    event_counter=0
-                    event= 'не найден'
-            dict_osc[osc.Index]={"name":osc.name,"info":dict_vers,"msg":dict_msg}
-            
-        print (dict_osc)
-        # break
+def Parsing_osc (root_path):
+    for osc  in files_osc.itertuples(): # перебор путей найденных файлов осцилограм
+        with open (osc.root+osc.name, 'rb') as file:
+            dict_osc[osc.Index]=RdfVersion1(file,osc.Index,osc.name)
+
+
+def RdfVersion1(file,Index,name):
+    dict_msg={}
+    #! засунуть проверку на вылет, когда не может декодировать rdf
+    dict_vers['extension']=file.read (4).decode('ANSI')
+    # print("формат=",dict_vers['extension'])
+    if dict_vers['extension']=="RDF":
+        dict_vers['v1']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v2']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v3']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v4']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v5']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v6']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['time']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        # print(dict_vers)
+        log=file.read (30050)
+        # сообщение в заголовке
+        title_log=int.from_bytes(file.read (1),byteorder='little',signed=0) #длинна байт
+        title_msg=file.read (title_log).decode(encoding='ANSI') # само сообщение
+        # отображение графиков
+        osc_graph_format_string_count=int.from_bytes(file.read (4),byteorder='little',signed=0) #читаем количество отоброжаемых окон графиков
+        for n in range(osc_graph_format_string_count): 
+            osc_graph_format_line_count = int.from_bytes(file.read (4),byteorder='little',signed=0) #количество графиков в окне
+            for m in range(osc_graph_format_line_count):
+                #? бесполезная для меня информация об отображении графиков, но посколку ее длинна переменна двумя циклами перебираем....
+                # dict_graph[n]=style
+                # dict_graph[n]['pbChannels'][m]=int.from_bytes(file.read (1),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].pbChannels[j]; 1b
+                # dict_graph[n]['pcrColor'][m]=int.from_bytes(file.read (4),byteorder='little',signed=0)     # ar>>pView->pCustomPD[i].pcrColor[j];    4b
+                # dict_graph[n]['piLineWidth'][m]=int.from_bytes(file.read (4),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].piLineWidth[j]; 4b
+                # dict_graph[n]['piLineStyle'][m]=int.from_bytes(file.read (4),byteorder='little',signed=0) # ar>>pView->pCustomPD[i].piLineStyle[j]; 4b
+                #?.... и пропускаем нужное количество байт
+                file.seek(file.tell()+13)
+            #? бесполезная пока инфа но переменная от цикла..
+            # fDigOnly=file.read (4)  # ar>>pView->pCustomPD[i].fDigOnly;   4b
+            # rect=file.read (16)       # ar>>pView->pCustomPD[i].rect;       4*4b
+            #? ... пропускаем
+            file.seek(file.tell()+20)
+        #? бесполезная пока инфа постоянной величины..
+        # m_fCaptionEnable=file.read (4) # ar>>pView->m_fCaptionEnable;
+        # m_fLegendEnable=file.read (4)  # ar>>pView->m_fLegendEnable;
+        # m_iTotalKadr=file.read (4)    # ar>>m_iTotalKadr;
+        # iAmountKadr=file.read (4)  # ar>>ViewData.iAmountKadr;
+        # iFromKadr= file.read (4) # ar>>ViewData.iFromKadr;
+        #? ... пропускаем
+        file.seek(file.tell()+20)
+        # парсим сообщения
+        m_iMessageCount=int.from_bytes(file.read (4),byteorder='little',signed=0) #количество сообщений
+        for m in range(m_iMessageCount):
+            dict_msg[m]={'num':0,'time':0,'msg':0,'style':0,'code':0}   
+            dict_msg[m]['num']=int.from_bytes(file.read (2),byteorder='little',signed=0)
+            dict_msg[m]['time']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+            dict_msg[m]['msg']=file.read (1).decode('ANSI')
+            dict_msg[m]['style']=file.read (1).decode('ANSI')
+            dict_msg[m]['code']=dict_code[int.from_bytes(file.read (1),byteorder='little',signed=0 )]
+            if dict_msg[m]['time'] == dict_vers['time']:
+                event_counter=1
+                event= dict_msg[m]['code']
+            elif dict_msg[m]['time']+1 == dict_vers['time']:
+                event_counter=2
+                event= dict_msg[m]['code']
+            elif dict_msg[m]['time']+2 == dict_vers['time']:
+                event_counter=3
+                event= dict_msg[m]['code']
+            else:
+                event_counter=0
+                event= 'не найден'
+        return {"name":name,"info":dict_vers,"msg":dict_msg}
+
+
+def RdfToDb(file,Index,name):
+    #! засунуть проверку на вылет, когда не может декодировать rdf
+    dict_vers['extension']=file.read (4).decode('ANSI')
+    if dict_vers['extension']=="RDF":
+        dict_vers['v1']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v2']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v3']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v4']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v5']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['v6']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        dict_vers['time']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+        
+        log=file.read (30050)
+        # сообщение в заголовке
+        title_log=int.from_bytes(file.read (1),byteorder='little',signed=0) #длинна байт
+        title_msg=file.read (title_log).decode(encoding='ANSI') # само сообщение
+        # отображение графиков
+        osc_graph_format_string_count=int.from_bytes(file.read (4),byteorder='little',signed=0) #читаем количество отоброжаемых окон графиков
+        for n in range(osc_graph_format_string_count): 
+            osc_graph_format_line_count = int.from_bytes(file.read (4),byteorder='little',signed=0) #количество графиков в окне
+            for m in range(osc_graph_format_line_count):
+                file.seek(file.tell()+13) #? ... пропускаем
+            file.seek(file.tell()+20) #? ... пропускаем
+        file.seek(file.tell()+20)#? ... пропускаем
+        # парсим сообщения
+        m_iMessageCount=int.from_bytes(file.read (4),byteorder='little',signed=0) #количество сообщений
+        for m in range(m_iMessageCount):
+            dict_msg[m]=msg
+            dict_msg[m]['num']=int.from_bytes(file.read (2),byteorder='little',signed=0)
+            dict_msg[m]['time']=int.from_bytes(file.read (4),byteorder='little',signed=0)
+            dict_msg[m]['msg']=file.read (1).decode('ANSI')
+            dict_msg[m]['style']=file.read (1).decode('ANSI')
+            dict_msg[m]['code']=dict_code[int.from_bytes(file.read (1),byteorder='little',signed=0 )]
+            if dict_msg[m]['time'] == dict_vers['time']:
+                event_counter=1
+                event= dict_msg[m]['code']
+            elif dict_msg[m]['time']+1 == dict_vers['time']:
+                event_counter=2
+                event= dict_msg[m]['code']
+            elif dict_msg[m]['time']+2 == dict_vers['time']:
+                event_counter=3
+                event= dict_msg[m]['code']
+            else:
+                event_counter=0
+                event= 'не найден'
+        dict_osc[Index]={"name":name,"info":dict_vers,"msg":dict_msg}
+
+
+if __name__ == '__main__':
+    files_osc = Getfiles(root_path,"rdf")   #составления списка осцилограмм
+    Parsing_osc (root_path)
+    print (dict_osc[1])
